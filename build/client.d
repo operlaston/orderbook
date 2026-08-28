b08d8388f6a6b8fceb7f4a64cf87bdfd0a8b3571
operlaston <operlaston21@gmail.com>
2026-08-28 13:07:53 -0500

    remove *.d files

--- build/client.d
+++
@@ -1,9 +0,0 @@
-build/client: src/client/Client.cpp include/GlobalConsts.h \
- include/MessageType.h include/OrderType.h include/Side.h \
- include/TimeInForce.h include/Utils.h
-include/GlobalConsts.h:
-include/MessageType.h:
-include/OrderType.h:
-include/Side.h:
-include/TimeInForce.h:
-include/Utils.h:
